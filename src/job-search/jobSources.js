0a3c28ade3c15f9171318067e4b8670d176288ff
binteriordesignpa-cyber <b.interiordesignpa@gmail.com>
2026-09-04 01:42:16 +0530

    Add Phase 2 job source configuration

---
+++ src/job-search/jobSources.js
@@ -0,0 +1,17 @@
+export const jobSources = [
+  {
+    name: "Indeed UAE",
+    type: "search_page",
+    query: "Senior Interior Designer Dubai"
+  },
+  {
+    name: "Bayt UAE",
+    type: "search_page",
+    query: "Senior Interior Designer Dubai"
+  },
+  {
+    name: "GulfTalent UAE",
+    type: "search_page",
+    query: "Senior Interior Designer Dubai"
+  }
+];
