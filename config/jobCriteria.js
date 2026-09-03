d78a9a680f7fbbc28bee5330568b31236d6004d4
binteriordesignpa-cyber <b.interiordesignpa@gmail.com>
2026-09-04 01:41:09 +0530

    feat: add candidate and Dubai job criteria

---
+++ config/jobCriteria.js
@@ -0,0 +1,22 @@
+export const candidate = {
+  experience: 13,
+  skills: [
+    "Interior Design",
+    "AutoCAD",
+    "SketchUp",
+    "V-Ray",
+    "Photoshop",
+    "Project Management",
+    "Shop Drawings",
+    "Procurement"
+  ]
+};
+
+export const targetCriteria = {
+  location: "Dubai",
+  roles: [
+    "Senior Interior Designer",
+    "Interior Designer",
+    "Interior Design Project Manager"
+  ]
+};
